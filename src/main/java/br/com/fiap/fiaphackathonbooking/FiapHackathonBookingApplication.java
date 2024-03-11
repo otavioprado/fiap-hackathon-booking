e6e0cb270f04467a6da8736932d4e3fd85bd7cd5
Otávio Prado <otavio.prado@sensedia.com>
2024-03-11 20:33:30 -0300

    criação do projeto

---
+++ src/main/java/br/com/fiap/fiaphackathonbooking/FiapHackathonBookingApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fiaphackathonbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiapHackathonBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiapHackathonBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/fiaphackathonbooking/FiapHackathonBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fiaphackathonbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiapHackathonBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
