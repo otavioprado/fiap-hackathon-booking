4ba72af0f71c510cf5e9ad4079af112f5f0d5515
Otávio Prado <otavio.prado@sensedia.com>
2024-03-11 22:00:53 -0300

    Criação do data.sql para inserir os dados na tabela

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO servico_opcional (nome, valor, tipo) VALUES ('Café da Manhã', 65.00, 'SERVICO');
+INSERT INTO servico_opcional (nome, valor, tipo) VALUES ('Almoço', 65.00, 'SERVICO');
+INSERT INTO servico_opcional (nome, valor, tipo) VALUES ('Jantar', 85.00, 'SERVICO');
+INSERT INTO servico_opcional (nome, valor, tipo) VALUES ('Massagem Completa', 250.00, 'SERVICO');
+INSERT INTO servico_opcional (nome, valor, tipo) VALUES ('Manicure', 85.00, 'SERVICO');
+
+INSERT INTO servico_opcional (nome, valor, tipo) VALUES ('Refrigerantes', 9.00, 'ITEM');
+INSERT INTO servico_opcional (nome, valor, tipo) VALUES ('Sucos', 13.00, 'ITEM');
+INSERT INTO servico_opcional (nome, valor, tipo) VALUES ('Cervejas Nacionais', 15.00, 'ITEM');
+INSERT INTO servico_opcional (nome, valor, tipo) VALUES ('Cervejas Importadas', 25.00, 'ITEM');
+INSERT INTO servico_opcional (nome, valor, tipo) VALUES ('Caipirinhas', 35.00, 'ITEM');
