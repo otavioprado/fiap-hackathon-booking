b0a4303f6ce9f231462133edfd3977e7dae0c27b
Kaique Ribeiro <57017906+kaiqueribe@users.noreply.github.com>
2024-03-13 21:39:55 -0300

    Correção Titulo e Descrição Swagger

--- src/main/java/br/com/fiap/fiaphackathonbooking/FiapHackathonBookingApplication.java
+++ src/main/java/br/com/fiap/fiaphackathonbooking/FiapHackathonBookingApplication.java
@@ -5,12 +5,12 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@OpenAPIDefinition(info = @Info(title = "Swagger OpenApi", version = "1", description = "Fiap-Hackathon-grupo-12"))
+@OpenAPIDefinition(info = @Info(title = "HACKATHON FIAP - BOOKING - Grupo-12", version = "1", description = "Este projeto é um sistema de gestão de hospitalidade, especificamente um sistema de reservas para hotéis. Ele permite aos usuários agendar, buscar a melhor solução custo-benefício e pré-reservar serviços e opções."))
 @SpringBootApplication
 public class FiapHackathonBookingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FiapHackathonBookingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FiapHackathonBookingApplication.class, args);
+    }
 
 }
